fbad44910cd90b1a488247a32b31e4bf4a3ec946
Saeedah123 <149072361+Saeedah123@users.noreply.github.com>
2024-02-14 07:02:45 +0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -137,10 +137,10 @@ def list_to_str(k):
     else:
         return ' '.join(f'{elem}, ' for elem in k)
 
-__repo__ = "https://github.com/MrMKN/PROFESSOR-BOT"
-__version__ = "PROFESSOR-BOT ᴠ4.5.0"
+__repo__ = "https://t.me/ProfessorMWBot"
+__version__ = "ProfessorMWBotᴠ4.5.0"
 __license__ = "GNU GENERAL PUBLIC LICENSE V2"
-__copyright__ = "Copyright (C) 2023-present MrMKN <https://github.com/MrMKN>"
+__copyright__ = "Copyright (C) 2023-present MrProfessor <https://t.me/ProfessorServers>"
 
 async def search_gagala(text):
     usr_agent = {
